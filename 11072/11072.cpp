9f695aa02b99ce89667312eed3c625047d140832
yh_Liverpool99 <yhmane@users.noreply.github.com>
2017-09-01 11:20:53 +0900

    11072

---
+++ 11072/11072.cpp
@@ -0,0 +1,37 @@
+//
+//  11072.cpp
+//  example
+//
+//  Created by hwangyunho on 2017. 9. 1..
+//  Copyright © 2017년 hwangyunho. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int a[1001];
+int d[1001];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int n, min, max=0;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+        cin>>a[i];
+    for(int i=1;i<=n;i++)
+    {
+        min=0;
+        for(int j=0;j<i;j++)
+        {
+            if(a[i]<a[j])
+            {
+                if(min<d[j])
+                    min=d[j];
+            }
+        }
+        d[i]=min+1;
+        if(max<d[i])
+            max=d[i];
+    }
+    cout<<max<<"\n";
+    return 0;
+}
