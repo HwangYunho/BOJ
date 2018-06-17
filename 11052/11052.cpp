47664d9cddf2e92eb84e4ee444f1952353b7a709
yhmane <hwang9yh@naver.com>
2018-06-17 19:43:08 +0900

    * dynamic programming > 11052

--- 11052/11052.cpp
+++ 11052/11052.cpp
@@ -2,25 +2,30 @@
 //  11052.cpp
 //  example
 //
-//  Created by hwangyunho on 2017. 8. 11..
-//  Copyright © 2017년 hwangyunho. All rights reserved.
+//  Created by hwangyunho on 2018. 6. 17..
+//  Copyright © 2018년 hwangyunho. All rights reserved.
 //
 
 #include <iostream>
 using namespace std;
 int cost[1001];
 int dp[1001];
-int main()
-{
-    ios_base::sync_with_stdio(false);
+
+int main() {
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++)
+    cin >> n;
+    
+    for(int i=1;i<=n;i++) {
         cin>>cost[i];
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=i;j++)
-            dp[i]=max(dp[i], dp[i-j]+cost[j]);
     }
-    cout<<dp[n]<<"\n";
+    
+    for(int i=1;i<=n; i++) {
+        for(int j=1;j<=i;j++) {
+            if (dp[i] < dp[i-j] + cost[j]) {
+                dp[i] = dp[i-j] + cost[j];
+            }
+        }
+    }
+    cout<<dp[n];
+    return 0;
 }
